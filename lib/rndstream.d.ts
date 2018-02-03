ecd15ef69da4bf937b0637f1b70a70fbf8a6a376
JLChnToZ <jeremylam619@gmail.com>
2018-02-03 17:29:45 +0800

    Add a more convenient way (Readable stream) to fetching random values

---
+++ lib/rndstream.d.ts
@@ -0,0 +1,55 @@
+import { Readable, ReadableOptions } from 'stream';
+import { JitterentropyFlags } from './wrapper';
+
+/**
+ * Buffered random pool from the results of Jitterentropy as a readable stream.
+ * Provide a more convenient, non-blocking way to use the random generator.
+ */
+export declare class RandomStream extends Readable {
+  /**
+   * Construct a buffered random pool.
+   * @param options Options passed to the undely readable stream
+   * @param osr Oversampling, default is `1` (No oversample)
+   * @param flags Flags controls the Jitterentropy behaviour
+   */
+  constructor(options?: ReadableOptions, osr?: number, flags?: JitterentropyFlags);
+
+  /**
+   * Pops a positive integer number from the random buffer.
+   * This method is the core of other helper functions which pops a number in this class.
+   * @param bytes how many bytes does the number requires, minumum is 1 and maximum is 6
+   * @returns A random positive integer number between 0 and (2^(bytes*8))
+   */
+  next(bytes: number): number;
+
+  /**
+   * Pops a float-point number between 0 and 1.
+   */
+  random(): number;
+
+  /**
+   * Pops a float-point number between 0 and the value given.
+   * @param max The number that generated number must not greater or equals to
+   */
+  random(max: number): number;
+
+  /**
+   * Pops a float-point number between the values given.
+   * @param min The number that generated number must not smaller to
+   * @param max The number that generated number must not greater or equals to
+   */
+  random(min: number, max: number): number;
+  
+  /**
+   * Pops an integer number between 0 and the value given.
+   * @param max The number that generated number must not greater or equals to
+   */
+  randomInt(max: number): number;
+
+  /**
+   * Pops an integer number between the values given.
+   * @param min The number that generated number must not smaller to
+   * @param max The number that generated number must not greater or equals to
+   */
+  randomInt(min: number, max: number): number;
+}
---
+++ lib/wrapper.d.ts
@@ -0,0 +1,111 @@
+/**
+ * Wrapper for Jitterentropy True Random Number Generator
+ */
+export declare class JitterTrng {
+  /**
+   * Create a new random number generator
+   * @param osr Controls oversampling, pass `1` to not oversample. Default is `1`.
+   * @param flags Internal flags changes the bahaviour of the TRNG. Default is `NONE`.
+   */
+  constructor(
+    osr?: number,
+    flags?: JitterentropyFlags
+  );
+
+  /**
+   * Collect some random bytes from the Jitterentropy with provided buffer.
+   * @param buf The buffer object to contain the random bytes.
+   * @param size How many bytes to be collected, maximum is the buffer size.
+   * @returns A number that indicates how many bytes has been collected to the buffer. Zero or negative values means something wrong is happened, please refer to mandoc `Jitterentropy(3)` for details.
+   */
+  read(buf: Buffer, size?: number): number;
+
+  readAsync(buf: Buffer, size?: number): Promise<number>;
+  readAsync(buf: Buffer, callback: (err: Error, count: number) => void): void;
+  readAsync(buf: Buffer, size: number, callback: (err: Error, count: number) => void): void;
+
+  /**
+   * Generate a float-point number between 0 and 1.
+   */
+  random(): number;
+
+  /**
+   * Generate a float-point number between 0 and the value given.
+   * @param max The number that generated number must not greater or equals to
+   */
+  random(max: number): number;
+
+  /**
+   * Generate a float-point number between the values given.
+   * @param low The number that generated number must not smaller to
+   * @param high The number that generated number must not greater or equals to
+   */
+  random(low: number, high: number): number;
+
+  randomAsync(): Promise<number>;
+  randomAsync(max: number): Promise<number>;
+  randomAsync(low: number, high: number): Promise<number>;
+  randomAsync(callback: (err: Error, result: number) => void): void;
+  randomAsync(max: number, callback: (err: Error, result: number) => void): void;
+  randomAsync(low: number, high: number, callback: (err: Error, result: number) => void): void;
+
+  /**
+   * Generate an integer number between 0 and the value given.
+   * @param max The number that generated number must not greater or equals to
+   */
+  randomInt(max: number): number;
+  
+  /**
+   * Generate an integer number between the values given.
+   * @param low The number that generated number must not smaller to
+   * @param high The number that generated number must not greater or equals to
+   */
+  randomInt(low: number, high: number): number;
+
+  randomIntAsync(max: number): Promise<number>;
+  randomIntAsync(low: number, high: number): Promise<number>;
+  randomIntAsync(max: number, callback: (err: Error, result: number) => void): void;
+  randomIntAsync(low: number, high: number, callback: (err: Error, result: number) => void): void;
+
+  /**
+   * Get a buffer that fills with random bytes.
+   * @param byteCount How many bytes should be in the buffer.
+   * @returns A buffer fill with random bytes, but keep in mind that it still has chance that entropy does not enough to fufill the count requested.
+   */
+  generate(byteCount: number): Buffer;
+
+  generateAsync(byteCount: number): Promise<Buffer>;
+  generateAsync(byteCount: number, callback: (err: Error, result: Buffer) => void): void;
+
+  /**
+   * Flag that indicates current instance is in use.
+   * While this is true, anyone want to gather bytes or numbers from this instance will throws error.
+   * It is because the undely mechanism is not thread-safe,
+   * attempt to use the same undely collector instance in parallel
+   * will interrupt the state and may crashes the whole process.
+   */
+  __inuse: boolean;
+}
+
+/**
+ * Flags that controls the behaviour of Jitterentropy TRNG.
+ * For the usage details please refer to the mandoc `Jitterentropy(3)`.
+ */
+export declare const enum JitterentropyFlags {
+  NONE = 0,
+  /**
+   * Disables theuse of the stirring function with the
+   * newly generated random number before it is given to the caller.
+   */
+  JENT_DISABLE_STIR = 1,
+  /**
+   * Disables the Von-Neumann unbias operation.
+   */
+  JENT_DISABLE_UNBIAS = 2,
+  /**
+   * Disables the allocation of thatmemory and therefore memory accesses.
+   * But that also implies that the entropycollection process onlyrelies
+   * on the complexity of the CPU.
+   */
+  JENT_DISABLE_MEMORY_ACCESS = 4,
+}
